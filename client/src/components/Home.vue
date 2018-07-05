9389c3c9ed323099677d0aa0e3b87967e14be7a2
jsusilo <enr@bk.ru>
2018-07-05 19:15:21 +0500

    Setup the home-template with buttons for navigation

---
+++ client/src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-layout>
+    <v-flex xs4>
+    </v-flex>
+    <v-flex xs8>
+      <div class="danger-alert" v-if="error">
+        {{error}}
+      </div>
+
+      <v-toolbar-items>
+        <v-btn
+          dark
+          :to="{
+            name: 'register'
+          }">
+          Регистрация
+        </v-btn>
+        &nbsp;
+        <v-btn
+          dark
+          :to="{
+            name: 'login'
+          }">
+          Войти
+        </v-btn>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <v-btn
+          dark
+          @click="logout">
+          Выйти
+        </v-btn>
+      </v-toolbar-items>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/components/Home'
 import Login from '@/components/Login'
 import Register from '@/components/Register'
 
@@ -7,6 +8,11 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
     {
       path: '/login',
       name: 'login',
