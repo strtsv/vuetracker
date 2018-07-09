6d31e1f5b4b3755c8786b63b079f594c9cbe1130
jsusilo <enr@bk.ru>
2018-07-09 12:22:20 +0500

    Setup the design of vuetify forms for login & register

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,17 +1,25 @@
 <template>
-  <v-layout column>
-    <v-flex xs6 offset-xs3>
-      <panel title="Login">
-        <v-text-field
-          label="Email"
-          v-model="email"
-        ></v-text-field>
-        <br>
-        <v-text-field
-          label="Password"
-          type="password"
-          v-model="password"
-        ></v-text-field>
+  <v-flex xs6 offset-xs3>
+    <div class="white elevation-5">
+      <v-toolbar flat dense class="cyan" dark>
+        <v-toolbar-title>Авторизация</v-toolbar-title>
+      </v-toolbar>
+      <div class="pl-4 pr-4 pt-3 pb-2">
+        <form
+          name="tab-tracker-form"
+          autocomplete="off">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
+          <br>
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+            autocomplete="new-password"
+          ></v-text-field>
+        </form>
         <br>
         <div class="danger-alert" v-html="error" />
         <br>
@@ -21,9 +29,9 @@
           @click="login">
           Войти
         </v-btn>
-      </panel>
-    </v-flex>
-  </v-layout>
+      </div>
+    </div>
+  </v-flex>
 </template>
 
 <script>
