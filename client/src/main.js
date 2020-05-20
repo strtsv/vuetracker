84214858e3b8a580ad380f78a2e573838d410d72
jsusilo <enr@bk.ru>
2020-05-21 00:22:36 +0500

    set up the youtube embed

--- client/src/main.js
+++ client/src/main.js
@@ -1,24 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import Vuetify from 'vuetify'
-import { sync } from 'vuex-router-sync'
-import 'vuetify/dist/vuetify.min.css'
-import store from '@/store/store'
-import Panel from '@/components/globals/Panel'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import Vuetify from "vuetify";
+import { sync } from "vuex-router-sync";
+import "vuetify/dist/vuetify.min.css";
+import store from "@/store/store";
+import VueYouTubeEmbed from "vue-youtube-embed";
+import Panel from "@/components/globals/Panel";
 
-Vue.component('panel', Panel)
+Vue.component("panel", Panel);
 
-Vue.use(Vuetify)
-sync(store, router)
+Vue.use(Vuetify);
+Vue.use(VueYouTubeEmbed);
+sync(store, router);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   store,
-  template: '<App/>',
+  template: "<App/>",
   components: { App }
-})
+});
