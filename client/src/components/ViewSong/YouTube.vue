d7140e4f91c7e4228f682735305848895d868776
jsusilo <enr@bk.ru>
2020-05-20 22:11:48 +0500

    set up the youtube embed

---
+++ client/src/components/ViewSong/YouTube.vue
@@ -0,0 +1,14 @@
+<template>
+  <panel title="YouTube Video">
+    <youtube :video-id="youtubeId" :player-width="800" :player-height="330">
+    </youtube>
+  </panel>
+</template>
+
+<script>
+export default {
+  props: ["youtubeId"]
+};
+</script>
+
+<style scoped></style>
