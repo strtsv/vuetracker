0182119d1a24f77f9762bcb238158f171096d838
jsusilo <enr@bk.ru>
2020-05-24 12:02:03 +0500

    metadata

---
+++ client/src/components/ViewSong/Lyrics.vue
@@ -0,0 +1,24 @@
+<template>
+  <panel title="Куплеты">
+    <textarea readonly v-model="song.lyrics"></textarea>
+  </panel>
+</template>
+
+<script>
+export default {
+  props: ["song"]
+};
+</script>
+
+<style>
+textarea {
+  width: 100%;
+  font-family: monospace;
+  border: none;
+  height: 600px;
+  border-style: none;
+  border-color: transparent;
+  overflow: auto;
+  padding: 40px;
+}
+</style>
--- client/src/components/ViewSong/SongMetadata.vue
+++ client/src/components/ViewSong/SongMetadata.vue
@@ -110,14 +110,27 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+
 .song-title {
   font-size: 30px;
 }
+
 .song-artist {
   font-size: 24px;
 }
+
 .song-genre {
   font-size: 18px;
+}
+
+.album-image {
+  width: 70%;
+  margin: 0 auto;
 }
 </style>
