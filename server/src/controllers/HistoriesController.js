9b0f545edf4807a1e7181ddb6bb940c885f09cd6
jsusilo <enr@bk.ru>
2020-05-29 00:04:24 +0500

    set up the song recently api

--- server/src/controllers/HistoriesController.js
+++ server/src/controllers/HistoriesController.js
@@ -2,6 +2,35 @@ const { History } = require("../models");
 const _ = require("lodash");
 
 module.exports = {
+  async index (req, res) {
+    try {
+      const userId = req.user.id
+      const histories = await History.findAll({
+        where: {
+          UserId: userId
+        },
+        include: [
+          {
+            model: Song
+          }
+        ],
+        order: [
+          ['createdAt', 'DESC']
+        ]
+      })
+        .map(history => history.toJSON())
+        .map(history => _.extend(
+          {},
+          history.Song,
+          history
+        ))
+      res.send(_.uniqBy(histories, history => history.SongId))
+    } catch (err) {
+      res.status(500).send({
+        error: 'an error has occured trying to fetch the history'
+      })
+    }
+  },
   async post(req, res) {
     try {
       const userId = req.user.id;
